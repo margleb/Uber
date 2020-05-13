ddfc3086688f3413c0500329abfc30a36653db5f
margleb <margleb@yandex.ru>
2020-05-13 19:49:39 +0300

    Saving User Profile Image In Firebase

--- app/src/main/java/com/project/uber/CustomerSettingsActivity.java
+++ app/src/main/java/com/project/uber/CustomerSettingsActivity.java
@@ -1,20 +1,40 @@
 package com.project.uber;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,11 +42,17 @@ public class CustomerSettingsActivity extends AppCompatActivity {
     private EditText mNameField, mPhoneField;
     private Button mBack, mConfirm;
 
+    private ImageView mProfileImage;
+
     private FirebaseAuth mAouth;
     private DatabaseReference mCustomerDatabase;
+
     private String userID;
     private String mName;
     private String mPhone;
+    private String mProfileImageUrl;
+
+    private Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +62,8 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         mNameField = (EditText) findViewById(R.id.name);
         mPhoneField = (EditText) findViewById(R.id.phone);
 
+        mProfileImage = (ImageView) findViewById(R.id.profileImage);
+
         mBack = (Button) findViewById(R.id.back);
         mConfirm = (Button) findViewById(R.id.confirm);
 
@@ -44,6 +72,15 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userID);
 
         getUserInfo();
+
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
 
         mConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +111,11 @@ public class CustomerSettingsActivity extends AppCompatActivity {
                         mPhone = map.get("phone").toString();
                         mPhoneField.setText(mPhone);
                     }
+                    if(map.get("profileImageUrl")!=null) {
+                        mProfileImageUrl = map.get("profileImageUrl").toString();
+                        // кеширует url изображения и помещает его область изображений
+                        Glide.with(getApplication()).load(mProfileImageUrl).into(mProfileImage);
+                    }
                 }
             }
 
@@ -94,7 +136,69 @@ public class CustomerSettingsActivity extends AppCompatActivity {
         userInfo.put("phone", mPhone);
         mCustomerDatabase.updateChildren(userInfo);
 
-        finish();
+        // если изображение загружено
+        if(resultUri != null) {
+            StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // создаем формат jpeg
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = filePath.putBytes(data);
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    finish();
+                    return;
+                }
+            });
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    // Get a URL на згружаемый контент
+                    StorageMetadata storageMetadata = taskSnapshot.getMetadata();
+                    StorageReference storageReference = storageMetadata.getReference();
+                    Task<Uri> taskUri = storageReference.getDownloadUrl();
+
+                    taskUri.addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            // Uri downloadUri = task.getResult();
+                            if(task.isSuccessful()) {
+                                Uri downloadUri = task.getResult();
+                                Map newImage = new HashMap();
+                                newImage.put("profileImageUrl", downloadUri.toString());
+                                mCustomerDatabase.updateChildren(newImage);
+                                finish();
+                                return;
+                            } else {
+                                // Task failed with an exception
+                                Exception exception = task.getException();
+                            }
+                        }
+                    });
+                }
+            });
+        } else {
+            finish();
+        }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1  && resultCode == Activity.RESULT_OK) {
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            mProfileImage.setImageURI(resultUri);
+        }
+    }
 }
