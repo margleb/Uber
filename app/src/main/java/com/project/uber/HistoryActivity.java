9f6277ba7ff63877625c0511e862d0e1971adde7
margleb <margleb@yandex.ru>
2020-05-16 14:55:59 +0300

    Populating Recycler View W/ FirebaseData

--- app/src/main/java/com/project/uber/HistoryActivity.java
+++ app/src/main/java/com/project/uber/HistoryActivity.java
@@ -1,11 +1,18 @@
 package com.project.uber;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.uber.historyRecyclerView.HistoryAdapter;
 import com.project.uber.historyRecyclerView.HistoryObject;
 
@@ -16,6 +23,7 @@ public class HistoryActivity extends AppCompatActivity {
     private RecyclerView mHistoryRecycleView;
     private RecyclerView.Adapter mHistoryAdapter;
     private RecyclerView.LayoutManager mHistoryLayoutManager;
+    private String customerOrDriver, userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,52 @@ public class HistoryActivity extends AppCompatActivity {
         mHistoryAdapter = new HistoryAdapter(getDataSetHistory(), HistoryActivity.this);
         mHistoryRecycleView.setAdapter(mHistoryAdapter);
 
-        for(int i=0; i< 100;i++) {
-            HistoryObject obj = new HistoryObject(Integer.toString(i));
-            resultHistory.add(obj);
-        }
-        mHistoryAdapter.notifyDataSetChanged();
+        customerOrDriver = getIntent().getExtras().getString("customerOrDriver");
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        getUserHistoryIds();
+
     }
+
+    private void getUserHistoryIds() {
+        DatabaseReference userHistoryDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(customerOrDriver).child(userId).child("history");
+        userHistoryDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    for(DataSnapshot history: dataSnapshot.getChildren()) {
+                        FetchRideInformation(history.getKey());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+    private void FetchRideInformation(String ridekey) {
+        DatabaseReference historyDatabase = FirebaseDatabase.getInstance().getReference().child("history").child(ridekey);
+        historyDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    String rideId = dataSnapshot.getKey();
+                    HistoryObject obj = new HistoryObject(rideId);
+                    resultHistory.add(obj);
+                    mHistoryAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
     private ArrayList resultHistory = new ArrayList<HistoryObject>();
     private ArrayList<HistoryObject> getDataSetHistory() {
